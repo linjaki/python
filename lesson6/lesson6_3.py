24235a3db012aee80cb24e7a25c4933207822f26
林家琪 <linjaki@gmail.com>
2025-05-15 13:32:53 +0000

    lesson6_3

--- lesson6/lesson6_3.py
+++ lesson6/lesson6_3.py
@@ -1,15 +1,4 @@
-def calculate_bmi(height:int, weight:int)->float:
-    if height < 120 or height > 220:
-            raise Exception(f'輸入的身高: {height} 公分 不在 120 ~ 220 範圍內')
-    height /= 100
-
-    if weight < 30.0 or weight > 200.0:
-            raise Exception(f'輸入的體重: {weight:.2f} 公斤 不在 30 ~ 200 範圍內')
-    
-    bmicalculate = weight / height ** 2
-    return bmicalculate
-
-def get_state(bmi:float)->str:
+import tools
     if bmi < 18.5:
         message = '你的體重過輕'
     elif bmi < 24:
